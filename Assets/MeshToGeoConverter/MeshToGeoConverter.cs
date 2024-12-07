b454052cd51fe9a4b51ec0cb3dc11d71822d13cf
HarukaKajita <kajitaj63b3@gmail.com>
2024-12-07 19:09:30 +0900

    重複頂点をまとめて書き出すオプション追加

--- Assets/MeshToGeoConverter/MeshToGeoConverter.cs
+++ Assets/MeshToGeoConverter/MeshToGeoConverter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,16 +10,18 @@ namespace MeshToGeoConverter
     public class MeshToGeoConverter
     {
         private MeshRenderer _meshRenderer;
-        private Mesh _mesh;
-        private FloatAttribute _pointAttribute; 
-        private FloatAttribute _normalAttribute; 
+        private Mesh _mesh; 
         public List<Attribute> PointAttributes = new();
+        public List<Attribute> VertexAttributes = new();
         public List<Attribute> PrimitiveAttributes = new();
-
-        public MeshToGeoConverter(MeshRenderer meshRenderer)
+        private int[] indices;
+        private int pointCount = 0;
+        bool outputNaively = false;
+        public MeshToGeoConverter(MeshRenderer meshRenderer, bool outputNaively = false, float weldThreshold = float.Epsilon)
         {
+            this.outputNaively = outputNaively;
             if (meshRenderer == null)
-                throw new System.ArgumentNullException(nameof(meshRenderer));
+                throw new ArgumentNullException(nameof(meshRenderer));
             
             // point attributes
             _meshRenderer = meshRenderer;
@@ -26,30 +29,86 @@ namespace MeshToGeoConverter
             _mesh = filter.sharedMesh;
             var verts = _mesh.vertices;
             var norms = _mesh.normals;
-            var positions = new float[verts.Length, 3];
-            var normals = new float[verts.Length, 3];
-            for (var i = 0; i < verts.Length; i++)
+            var positions = new float[,]{};
+            var normals = outputNaively ? new float[verts.Length, 3] : new float[_mesh.triangles.Length, 3];
+            indices = _mesh.triangles;
+
+            var distinctPositions = new List<Vector3>();
+            var remappedIndices = new int[verts.Length];
+            if (!outputNaively)
             {
-                positions[i, 0] = -verts[i].x;
-                positions[i, 1] = verts[i].y;
-                positions[i, 2] = verts[i].z;
-                
-                normals[i, 0] = -norms[i].x;
-                normals[i, 1] = norms[i].y;
-                normals[i, 2] = norms[i].z;
+                // 頂点座標の重複を削除
+                for (var i = 0; i < verts.Length; i++)
+                {
+                    var alreadyExists = false;
+                    for (var index = 0; index < distinctPositions.Count; index++)
+                    {
+                        var p = distinctPositions[index];
+                        if (weldThreshold < Math.Abs(p.x - verts[i].x)) continue;
+                        if (weldThreshold < Math.Abs(p.y - verts[i].y)) continue;
+                        if (weldThreshold < Math.Abs(p.z - verts[i].z)) continue;
+                        // ほぼ同値の場合
+                        remappedIndices[i] = index;
+                        alreadyExists = true;
+                        break;
+                    }
+                    if (alreadyExists) continue;
+
+                    remappedIndices[i] = distinctPositions.Count;
+                    distinctPositions.Add(verts[i]);
+                }
+                // distinctPositionsをpositionsにコピー
+                pointCount = distinctPositions.Count;
+                positions = new float[pointCount, 3];
+                for (var i = 0; i < distinctPositions.Count; i++)
+                {
+                    positions[i, 0] = -distinctPositions[i].x;
+                    positions[i, 1] = distinctPositions[i].y;
+                    positions[i, 2] = distinctPositions[i].z;
+                }
+                // 頂点法線のコピー
+                for (var i = 0; i < indices.Length; i++)
+                {   
+                    normals[i, 0] = -norms[indices[i]].x;
+                    normals[i, 1] = norms[indices[i]].y;
+                    normals[i, 2] = norms[indices[i]].z;
+                }
+                // remap indices
+                for (var i = 0; i < indices.Length; i++)
+                    indices[i] = remappedIndices[indices[i]];
             }
-            _pointAttribute = new FloatAttribute("P", positions);
-            _normalAttribute = new FloatAttribute("N", normals);
-            PointAttributes.Add(_normalAttribute);
+            else
+            {
+                pointCount = verts.Length;
+                positions = new float[pointCount, 3];
+                // 頂点座標の重複を削除せずにそのままコピー
+                for (var i = 0; i < pointCount; i++)
+                {
+                    positions[i, 0] = -verts[i].x;
+                    positions[i, 1] = verts[i].y;
+                    positions[i, 2] = verts[i].z;
+                }
+                // 頂点法線のコピー
+                for (var i = 0; i < pointCount; i++)
+                {   
+                    normals[i, 0] = -norms[i].x;
+                    normals[i, 1] = norms[i].y;
+                    normals[i, 2] = norms[i].z;
+                }
+            }
+            
+            // 頂点座標アトリビュート
+            PointAttributes.Add(new FloatAttribute("P", positions));
+            
+            // 頂点法線アトリビュート
+            if(outputNaively) PointAttributes.Add(new FloatAttribute("N", normals));
+            else  VertexAttributes.Add(new FloatAttribute("N", normals));
             
             // primitive attributes
             var subMeshTriangleCount = new int[_mesh.subMeshCount];
             var subMeshCount = _mesh.subMeshCount;
             for (var i = 0; i < subMeshCount; i++)
-            {
-                var submesh = _mesh.GetSubMesh(i);
-                subMeshTriangleCount[i] = submesh.indexCount / 3;
-            }
+                subMeshTriangleCount[i] = _mesh.GetSubMesh(i).indexCount / 3;
         
             var triangleCount = _mesh.triangles.Length / 3;
             var materials = new List<string>(triangleCount);
@@ -67,7 +126,7 @@ namespace MeshToGeoConverter
                     subMeshId[j,0] = i;
                 }
             }
-            var materialAttribute = new StringAttribute("materialName");
+            var materialAttribute = new StringAttribute("shop_materialpath");
             var meshNameAttribute = new StringAttribute("meshName");
             var subMeshIdAttribute = new IntAttribute("subMeshId", subMeshId);
             materialAttribute.SetValues(materials.ToArray());
@@ -105,7 +164,7 @@ namespace MeshToGeoConverter
 
         private void MakeContent(StringBuilder sb)
         {
-            sb.AppendLine($"\"pointcount\",{_mesh.vertexCount},");
+            sb.AppendLine($"\"pointcount\",{pointCount},");
             sb.AppendLine($"\"vertexcount\",{_mesh.triangles.Length},");
             sb.AppendLine($"\"primitivecount\",{_mesh.triangles.Length/3},");
             MakeTopologyContent(sb);
@@ -115,7 +174,6 @@ namespace MeshToGeoConverter
 
         private void MakeTopologyContent(StringBuilder sb)
         {
-            var indices = _mesh.triangles;
             sb.AppendLine("\"topology\",[");
             sb.AppendLine("\"pointref\",[");
             sb.AppendLine($"\"indices\",[{string.Join(",", indices)}]");
@@ -127,31 +185,30 @@ namespace MeshToGeoConverter
         {
             sb.AppendLine("\"attributes\",[");
             
+                if(0 < VertexAttributes.Count)
+                {
+                    sb.AppendLine("\"vertexattributes\",[");
+                    MakeAttributeContent(sb, VertexAttributes);
+                    sb.AppendLine("],");
+                }
+                
                 sb.AppendLine("\"pointattributes\",[");
-                MakePointAttributeContent(sb);
+                MakeAttributeContent(sb, PointAttributes);
                 sb.AppendLine("],");
-
-            if (0 < PrimitiveAttributes.Count)
-            {
-                sb.AppendLine("\"primitiveattributes\",[");
-                MakePrimitiveAttributeContent(sb);
-                sb.AppendLine("],");
-            }
-
+                
+                if (0 < PrimitiveAttributes.Count)
+                {
+                    sb.AppendLine("\"primitiveattributes\",[");
+                    MakeAttributeContent(sb, PrimitiveAttributes);
+                    sb.AppendLine("],");
+                }
             
             sb.AppendLine("],");
         }
 
-        private void MakePointAttributeContent(StringBuilder sb)
+        private void MakeAttributeContent(StringBuilder sb, IEnumerable<Attribute> attributes)
         {
-            sb.AppendLine(_pointAttribute.MakeJson());
-            foreach (var attribute in PointAttributes)
-                sb.AppendLine(attribute.MakeJson());
-        }
-
-        private void MakePrimitiveAttributeContent(StringBuilder sb)
-        {
-            foreach (var attribute in PrimitiveAttributes)
+            foreach (var attribute in attributes)
                 sb.AppendLine(attribute.MakeJson());
         }
 
